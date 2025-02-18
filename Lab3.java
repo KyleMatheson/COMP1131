6416b99133c9628b468bc86d1ecde4bccbb47e1e
Kyle Matheson <Matheson@mcsquared.com>
2025-02-18 00:02:10 -0800

    Delete garbage files

--- Lab3.java
+++
@@ -1,9 +0,0 @@
-public class Lab3
-{
-    public static void main (String[] args)
-    {
-        String introduction = "123456789111111";
-
-        System.out.println(introduction.substring(7,8));
-    }
-}
