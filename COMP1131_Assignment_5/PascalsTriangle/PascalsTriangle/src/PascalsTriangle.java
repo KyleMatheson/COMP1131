9cefd5806e0b30b306a80f47e53771f6b084ab1e
Kyle Matheson <kyle.matheson@bcdisplay.com>
2025-04-14 13:19:17 -0700

    Iterative structure complete.  Need to add recursive function.

--- COMP1131_Assignment_5/PascalsTriangle/PascalsTriangle/src/PascalsTriangle.java
+++ COMP1131_Assignment_5/PascalsTriangle/PascalsTriangle/src/PascalsTriangle.java
@@ -5,34 +5,27 @@ public class PascalsTriangle
 {
     public static void main (String[] args)
     {
-        int[] value = new int[2];
-
-
-
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter the number of rows for your Pascal's Triangle:");
-        int r = scan.nextInt();
-        System.out.print("c:");
-        int c = scan.nextInt();
+        int rows = scan.nextInt();
 
-        for (int i = 0; i<r; i++) //row iterator
+        int row;
+        int column;
+        for (row = 0; row < rows; row++)
         {
-
-            int array = calc(i,c) + calc(i,c);
-            System.out.println();
+            for (column = 0; column <= row; column++)
+            {
+                int value = calc(row, column);
+                System.out.printf("(%d,%d) : %d)",row,column,value);
+            }
         }
 
-
-        //value = calc(r-1, c-1) + calc(r-1, 1);
-        System.out.println();
 
     }
 
     private static int calc(int r, int c)
     {
-
-        int[] calcValue = {((r - 1) + (r - 1)),((c-1)+ c)};
-        return calcValue[0];
+        return 1;
     }
 }
